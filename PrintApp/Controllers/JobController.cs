dc8a007fca4987e938902a999da9e962bba22d1a
goran-kovacic <132432118+goran-kovacic@users.noreply.github.com>
2024-04-09 19:22:19 +0200

    gotov job controller

--- PrintApp/Controllers/JobController.cs
+++ PrintApp/Controllers/JobController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PrintApp.Data;
 using PrintApp.Mappers;
 using PrintApp.Models;
@@ -30,6 +31,48 @@ namespace PrintApp.Controllers
             entity.Printer = printer;
             entity.Material = material;
             entity.Part = part;
+
+            return entity;
+        }
+
+        protected override List<JobDTORead> UcitajSve()
+        {
+            var list = _context.PrintJobs
+                .Include(p => p.Part)
+                .Include(p => p.Material)
+                .Include(p => p.Printer)
+                .ToList();
+            if(list == null || list.Count == 0)
+            {
+                throw new Exception("nema podataka u bazi");
+            }
+            return _mapper.MapReadList(list);
+        }
+
+        protected override PrintJob NadiEntitet(int id)
+        {
+            return _context.PrintJobs
+                .Include(p => p.Part)
+                .Include(p => p.Material)
+                .Include(p => p.Printer)
+                .FirstOrDefault(x => x.Id == id)
+                ?? throw new Exception("ne postoji job sa sifrom " + id + " u bazi");
+        }
+
+        protected override PrintJob PromjeniEntitet(JobDTOInsertUpdate dto, PrintJob entity)
+        {
+            var part = _context.Parts.Find(dto.PartId)
+                ?? throw new Exception("Ne postoji part sa šifrom " + dto.PartId + " u bazi");
+            var material = _context.Materials.Find(dto.MaterialId)
+                ?? throw new Exception("Ne postoji material sa šifrom " + dto.MaterialId + " u bazi");
+            var printer = _context.Printers.Find(dto.PrinterId)
+                ?? throw new Exception("Ne postoji printer sa šifrom " + dto.PrinterId + " u bazi");
+
+            entity.Result = dto.Result;
+            entity.Volume = dto.Volume;
+            entity.Material = material;
+            entity.Part = part;
+            entity.Printer = printer;
 
             return entity;
         }
