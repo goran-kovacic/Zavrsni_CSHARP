01ec7ee37ae1bf5f6de023c53d18550a23119c9f
goran-kovacic <kovacic.go@gmail.com>
2024-04-09 18:34:24 +0200

    dovrsiti job controller

---
+++ PrintApp/Controllers/JobController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrintApp.Data;
+using PrintApp.Mappers;
+using PrintApp.Models;
+
+namespace PrintApp.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class JobController:AppController<PrintJob, JobDTORead, JobDTOInsertUpdate>
+    {
+        public JobController(PrintAppContext context) : base(context) 
+        {
+            DbSet = _context.PrintJobs;
+            _mapper = new MappingJobs();
+        }
+
+        protected override void ControlDelete(PrintJob entity)
+        {
+           
+        }
+
+        protected override PrintJob KreirajEntitet(JobDTOInsertUpdate dto)
+        {
+            var printer = _context.Printers.Find(dto.PrinterId);
+            var material = _context.Materials.Find(dto.MaterialId);
+            var part = _context.Parts.Find(dto.PartId);
+            var entity = _mapper.MapInsertUpdatedFromDTO(dto);
+
+            entity.Printer = printer;
+            entity.Material = material;
+            entity.Part = part;
+
+            return entity;
+        }
+    }
+}
